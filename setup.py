e21bd52a7f5b9a5e87312a7db0212cc33b3d404c
ImalAdhikari <eg244916@engug.ruh.ac.lk>
2026-06-21 22:13:37 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    
+name='Analysis-Model',
+version='0.1.0',
+packages=find_packages(),
+author_email = 'eg244916@engug.ruh.ac.lk',
+author = 'eg244916'
+install_requires=['pandas', 'numpy', 'scikit-learn', 'matplotlib', 'seaborn']
+
+)
